6f81fdaf5ec6dba5cda7365f4220a31d9501a660
Outerblog <sunjack80@gmail.com>
2024-08-01 20:39:37 +1200

    Quartz sync: Aug 1, 2024, 8:39 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "Discord Username": "https://discordapp.com/users/901089713253937213/",
+      "Join my Minecraft server 😉": "https://discord.gg/pGr9fQvuGn",
     },
   }),
 }
